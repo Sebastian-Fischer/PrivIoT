915bfd66ebbe94e0a5221621c2e8ce4259eaf34e
Sebastian-Fischer <Sebastian-Fischer@github.com>
2014-09-13 12:08:27 +0200

    Created executable class for generating secrets

---
+++ PrivIoT-utils/src/main/java/de/uniluebeck/itm/priviot/utils/GenerateSecretMain.java
@@ -0,0 +1,26 @@
+package de.uniluebeck.itm.priviot.utils;
+
+import de.uniluebeck.itm.priviot.utils.pseudonymization.PseudonymizationException;
+import de.uniluebeck.itm.priviot.utils.pseudonymization.Secret;
+
+public class GenerateSecretMain {
+
+	public static void main(String[] args) {
+		System.out.println("PrivIoT Utils - Generation of Secret");
+		
+		System.out.println("This will generate a pseudorandom secret for the use in a pseudonym");
+		
+		byte[] secret;
+		try {
+			secret = PseudonymizationProcessor.generateHmac256Secret();
+		} catch (PseudonymizationException e) {
+			System.out.println("Error during generation of secret: " + e.getMessage());
+			return;
+		}
+		
+		String secretStr = Secret.encodeBase64Secret(secret);
+		
+		System.out.println("The secret (as Base64 encoded string) is:\n" + secretStr);
+	}
+
+}
